c481b3d6e291d62c743caa744fb4dd402c2bccae
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2016-05-22 12:45:46 +0200

    add link to source

--- bin/pair_with.sh
+++ bin/pair_with.sh
@@ -1,3 +1,7 @@
+
+
+# for more information, see http://alvarogarcia7.github.io/blog/2016/02/14/two-persons-involved-in-a-git-commit/
+
 echo "export GIT_COMMITTER_NAME=person_name"
 echo "export GIT_COMMITTER_EMAIL=person_email"
 
