35621039006f42162eccd235964706534ea1431a
alvaro garcia <derecerca@gmail.com>
2016-06-15 10:31:41 +0100

    refactor: move pry configuration to its own folder

--- install.sh
+++ install.sh
@@ -109,8 +109,9 @@ install_dotfiles () {
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 3 -name '*.symlink' -not -path '*.git*')
   do
+    echo "found $src"
     dst="$HOME/.$(basename "${src%.*}")"
     link_file "$src" "$dst"
   done
