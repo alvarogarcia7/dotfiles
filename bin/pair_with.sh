0e3fdf5ad30a1c7f0e28b9e4ae8618a0c3641127
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2016-06-15 11:10:15 +0200

    explain why this is not functional

--- bin/pair_with.sh
+++ bin/pair_with.sh
@@ -2,6 +2,8 @@
 
 # for more information, see http://alvarogarcia7.github.io/blog/2016/02/14/two-persons-involved-in-a-git-commit/
 
+echo "as this might contain personal information, we only show how to do it. Copy/paste with correct data"
+
 echo "export GIT_COMMITTER_NAME=person_name"
 echo "export GIT_COMMITTER_EMAIL=person_email"
 
