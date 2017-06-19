d18fb77cb44c82b70c36dc1ce430faa548ec499e
Alvaro Garcia <derecerca@gmail.com>
2017-06-19 11:05:43 +0200

    Refactor: move pair_with_ to .zshrc

--- bin/pair_with.sh
+++ bin/pair_with.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# for more information, see http://alvarogarcia7.github.io/blog/2016/02/14/two-persons-involved-in-a-git-commit/
+echo "for more information, see http://alvarogarcia7.github.io/blog/2016/02/14/two-persons-involved-in-a-git-commit/"
+echo "to check the last commits: git log --format=\"%aN %aE %cN %cE\" "
+echo "find a function named 'pair_with_' in your .zshrc"
 
-echo "as this might contain personal information, we only show how to do it. Copy/paste with correct data"
-
-echo "export GIT_COMMITTER_NAME=person_name"
-echo "export GIT_COMMITTER_EMAIL=person_email"
-
-echo "to check the last commits: git log --format=\"%aN %aE %cN %cE"\   "
