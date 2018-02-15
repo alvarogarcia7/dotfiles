9e73f72fa10f51e19f3a54883d0891b02c01aad0
Alvaro Garcia <derecerca@gmail.com>
2018-02-15 19:05:55 +0100

    #3 - No need to output debug variables everytime

--- bin/commitatdate.sh
+++ bin/commitatdate.sh
@@ -3,13 +3,10 @@
 set -e
 
 if [ -z ${GIT_DATE_DELTA+x} ]; then
-    echo "var is unset"
+    echo "date delta not set"
 else 
     export GIT_COMMITTER_DATE=$(date -v $GIT_DATE_DELTA)
     export GIT_AUTHOR_DATE=$(date -v $GIT_DATE_DELTA)
-    echo "dates are now set"
-    echo "GIT_COMMITTER_DATE=$GIT_COMMITTER_DATE" 
-    echo "GIT_AUTHOR_DATE=$GIT_AUTHOR_DATE" 
 fi
 
 git commit "$@"
