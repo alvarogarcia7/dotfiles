ea09e8c5b09acc3bd842c21f7a1961e21e936fa8
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-04-18 01:57:57 +0200

    Change rtorrent port (collides with spring boot default)

--- bin/rtorrent.sh
+++ bin/rtorrent.sh
@@ -1,1 +1,1 @@
-docker run -dt  -p 8080:80 -p 49160:49160/udp -p 49161:49161 -v ~/test:/downloads diameter/rtorrent-rutorrent:stable-alpine
+docker run -dt  -p 8088:80 -p 49160:49160/udp -p 49161:49161 -v ~/test:/downloads diameter/rtorrent-rutorrent:stable-alpine
