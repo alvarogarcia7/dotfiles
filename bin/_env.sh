f64285c543bc984ffd794da0b3586fb47612e4df
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-04-18 01:57:30 +0200

    More documentation on .env.sh

--- bin/_env.sh
+++ bin/_env.sh
@@ -3,3 +3,6 @@
 
 java -version 2>&1|grep -q "11\.0\.1" || sdk use java "11.0.1-zulu"
 
+## Setting your email for a specific folder (i.e., client-specific)
+export GIT_COMMITTER_EMAIL="desired_email@domain.com"
+export GIT_AUTHOR_EMAIL="$GIT_COMMITTER_EMAIL"
