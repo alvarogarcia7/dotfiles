bf119ffa4c6d9449199678b7726d3f36d055e8e8
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-05-27 12:22:37 +0200

    [Sublime] Copy all the user folder to dotfiles

--- sublime/keymap.py
+++
@@ -1,6 +0,0 @@
-// Install the package manager, then install the package "Insert Date", by "FichteFoll"
-// Open the key bindings
-[
-	//https://github.com/FichteFoll/InsertDate#snippet-macros
-	{"keys": ["f5"], "command": "insert_date", "args": {"format": "%Y-%m-%d %H:%M:%S AGB"}},
-]
