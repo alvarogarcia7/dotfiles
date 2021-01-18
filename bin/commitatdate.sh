161779369e27ba845ccf5268246cc4d453b8de51
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-01-18 09:05:54 +0400

    [Bin] Defect: using an extra $, cannot define a constant

--- bin/commitatdate.sh
+++ bin/commitatdate.sh
@@ -9,14 +9,14 @@ else
   # Source: https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then #linux
     # An example:
-    # export $GIT_DATE_DELTA="2 hours ago"
+    # export GIT_DATE_DELTA="2 hours ago"
     export GIT_COMMITTER_DATE=$(date --date="$GIT_DATE_DELTA")
     export GIT_AUTHOR_DATE=$(date --date="$GIT_DATE_DELTA")
   elif [[ "$OSTYPE" == "darwin"* ]]; then # Mac OSX
     # An example:
-    # export $GIT_DATE_DELTA="-2H"
-    export GIT_COMMITTER_DATE=$(date -v $GIT_DATE_DELTA)
-    export GIT_AUTHOR_DATE=$(date -v $GIT_DATE_DELTA)
+    # export GIT_DATE_DELTA="-2H"
+    export GIT_COMMITTER_DATE=$(date -v "$GIT_DATE_DELTA")
+    export GIT_AUTHOR_DATE=$(date -v "$GIT_DATE_DELTA")
   else
     echo "This script is not supported for this operating system '$OSTYPE'"
     exit -1 
